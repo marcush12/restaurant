f894f63459370cedfd5fd6a99450cacbe3780ae8
marcush12 <marcos012santos@gmail.com>
2020-03-17 12:09:59 -0300

    first commit

---
+++ src/components/layout.js
@@ -0,0 +1,33 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { createGlobalStyle } from "styled-components"
+import Navbar from '../components/globals/navbar'
+// import './bootstrap.min.css'
+// import "./layout.css"
+
+const Layout = ({ children }) => {
+  return <React.Fragment>
+    <GlobalStyle />
+    <Navbar />
+    {children}
+    </React.Fragment>
+}
+
+const GlobalStyle = createGlobalStyle`
+  * {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: 'Open Sans', sans-serif;
+  color: #262626;
+  background: #fff;
+}
+`
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <h3>hello from index page!</h3>
+  </Layout>
+)
+
+export default IndexPage
